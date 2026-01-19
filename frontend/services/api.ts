242ca76b46a3ac1af6e69e9fa782957b4b885752
th-herve <126357296+th-herve@users.noreply.github.com>
2026-01-19 09:44:42 +0100

    chore(front): add the backend url in a .env file

--- frontend/services/api.ts
+++ frontend/services/api.ts
@@ -1,7 +1,6 @@
 import axios from "axios";
 
-// TODO .env
-const BASE_URL = "http://localhost:8080";
+const BASE_URL = process.env.BACKEND_API_URL;
 
 export const api = axios.create({
   baseURL: BASE_URL,
