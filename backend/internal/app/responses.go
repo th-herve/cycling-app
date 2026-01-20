9e82bec44bb6950d4a4e10db17dcc010c9bd4cf2
th-herve <126357296+th-herve@users.noreply.github.com>
2026-01-20 15:03:07 +0100

    feat(back): add rider and result response struct

--- backend/internal/app/responses.go
+++ backend/internal/app/responses.go
@@ -3,6 +3,10 @@ package app
 import (
 	"cycling-backend/internal/common"
 	"cycling-backend/internal/domain/event"
+	"cycling-backend/internal/domain/result"
+	"cycling-backend/internal/domain/rider"
+
+	"github.com/google/uuid"
 )
 
 type EventResponse struct {
@@ -10,4 +14,47 @@ type EventResponse struct {
 	Country    *common.CountrySnapshot `json:"country,omitempty"`
 	Stages     []*EventResponse        `json:"stages,omitempty"`
 	ParentName *string                 `json:"parentName,omitempty"`
+	Results    *ResultsSnapshot        `json:"results,omitempty"`
+}
+
+type ResultsSnapshot struct {
+	General  []ResultSnapshot `json:"general,omitempty"`
+	Mountain []ResultSnapshot `json:"mountain,omitempty"`
+	Point    []ResultSnapshot `json:"point,omitempty"`
+}
+
+type ResultSnapshot struct {
+	Rank  int16         `json:"rank"`
+	Rider RiderSnapshot `json:"rider"`
+}
+
+func ResultToSnapshot(result result.Result) ResultSnapshot {
+	snapshot := ResultSnapshot{}
+	if result.Rank != nil {
+		snapshot.Rank = *result.Rank
+	}
+	return snapshot
+}
+
+type RiderSnapshot struct {
+	ID          uuid.UUID               `json:"id"`
+	FirstName   string                  `json:"firstName"`
+	LastName    string                  `json:"lastName"`
+	Nationality *common.CountrySnapshot `json:"nationality,omitempty"`
+	Team        *TeamSnapshot           `json:"team,omitempty"`
+}
+
+func RiderToSnapshot(rider rider.Rider) RiderSnapshot {
+	return RiderSnapshot{
+		ID: rider.ID,
+		FirstName: rider.FirstName,
+		LastName: rider.LastName,
+	}
+}
+
+type TeamSnapshot struct {
+	ID           uuid.UUID               `json:"id"`
+	Name         string                  `json:"name"`
+	Abbreviation string                  `json:"abbreviation"`
+	Country      *common.CountrySnapshot `json:"country,omitempty"`
 }
