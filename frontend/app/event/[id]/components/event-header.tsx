75de4771d0bf3afd31c4325f3a0f61294e4ab1a6
th-herve <126357296+th-herve@users.noreply.github.com>
2026-06-18 15:39:43 +0200

    feat(front): use flex container for event detail title
    On mobile, this separate the flag and the name. So if the name text goes
    onto the next line, it align with the name first line, not the flag.

--- frontend/app/event/[id]/components/event-header.tsx
+++ frontend/app/event/[id]/components/event-header.tsx
@@ -7,9 +7,9 @@ export const EventHeader = ({ event }: { event: Event }) => {
   const isCanceled = event.status === "canceled";
   return (
     <>
-      <h1>
+      <h1 className="flex items-start">
         <CountryIcon
-          className="mr-4"
+          className="mr-4 pt-1"
           countryCode={event.country?.alpha2 || ""}
           aria-label={event.country?.name}
         />
