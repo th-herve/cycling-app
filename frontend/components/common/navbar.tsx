2d6640099303e5791068667d1b3bd1bac2e807b5
th-herve <126357296+th-herve@users.noreply.github.com>
2026-07-05 11:28:01 +0200

    refactor(front): better navbar semantic

--- frontend/components/common/navbar.tsx
+++ frontend/components/common/navbar.tsx
@@ -9,21 +9,40 @@ interface Props {
 
 const Navbar = ({ className }: Props) => {
   return (
-    <div
-      className={cn("bg-background flex w-full items-center py-5 gap-10", className)}
-    >
-      <Link href={siteRoute.schedule.calendar}>
-        <div className="flex items-center gap-2">
-          <Image src="/icon" alt="icon" width={32} height={32} />
-          <p className="font-title">Cycling Calendar</p>
-        </div>
-      </Link>
+    <header>
+      <nav
+        aria-label="Main navigation"
+        className={cn(
+          "bg-background flex w-full items-center gap-10 py-5",
+          className,
+        )}
+      >
+        <Link href={siteRoute.schedule.calendar}>
+          <div className="flex items-center gap-2">
+            <Image
+              src="/icon"
+              alt="Cycling Calendar logo"
+              width={32}
+              height={32}
+            />
+            <span className="font-title">Cycling Calendar</span>
+          </div>
+        </Link>
 
-      <div className="flex items-center gap-5">
-        <Link className="font-bold" href={siteRoute.schedule.calendar}>Calendar</Link>
-        <Link className="font-bold" href={siteRoute.team.list}>Teams</Link>
-      </div>
-    </div>
+        <ul className="flex items-center gap-5">
+          <li>
+            <Link className="font-bold" href={siteRoute.schedule.calendar}>
+              Calendar
+            </Link>
+          </li>
+          <li>
+            <Link className="font-bold" href={siteRoute.team.list}>
+              Teams
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
   );
 };
 
