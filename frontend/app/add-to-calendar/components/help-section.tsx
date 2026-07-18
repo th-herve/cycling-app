ae7ccdf8dc58ef4a0fea32c46ba64cc0b0044f3e
th-herve <126357296+th-herve@users.noreply.github.com>
2026-07-18 15:15:24 +0200

    feat(front): add Proton Calendar help in export page

--- frontend/app/add-to-calendar/components/help-section.tsx
+++ frontend/app/add-to-calendar/components/help-section.tsx
@@ -11,61 +11,97 @@ const HelpSection = () => {
   return (
     <div className="max-w-200">
       <h2 className="mb-4">Help</h2>
-      <Collapsible>
-        <CollapsibleTrigger asChild>
-          <Button variant="ghost" className="group w-full text-base">
-            Google Calendar
-            <FaChevronDown className="ml-auto group-data-[state=open]:rotate-180" />
-          </Button>
-        </CollapsibleTrigger>
-
-        <CollapsibleContent className="text-muted-foreground px-5">
-          If the Google Calendar button doesn&apos;t work, you can subscribe
-          manually:
-          <ol className="ml-8 list-decimal">
-            <li>Open google calendar.</li>
-            <li>
-              Click <Badge variant="outline">+</Badge> next to{" "}
-              <Badge variant="outline">Other calendars</Badge>.
-            </li>
-            <li>
-              Select <Badge variant="outline">From URL</Badge>.
-            </li>
-            <li>Paste the calendar URL above.</li>
-            <li>
-              Click <Badge variant="outline">Add calendar</Badge>.
-            </li>
-          </ol>
-        </CollapsibleContent>
-      </Collapsible>
-
-      <Collapsible>
-        <CollapsibleTrigger asChild>
-          <Button variant="ghost" className="group w-full text-base">
-            Outlook
-            <FaChevronDown className="ml-auto group-data-[state=open]:rotate-180" />
-          </Button>
-        </CollapsibleTrigger>
-        <CollapsibleContent className="text-muted-foreground px-5">
-          If the Outlook button doesn&apos;t work (which is usually the case if
-          you are using the web version), you can subscribe manually:
-          <ol className="ml-8 list-decimal">
-            <li>Open Outlook Calendar.</li>
-            <li>
-              Click <Badge variant="outline">Add calendar</Badge>.
-            </li>
-            <li>
-              Click <Badge variant="outline">Subscribe from web</Badge>.
-            </li>
-            <li>Paste the calendar URL above.</li>
-            <li>
-              Click <Badge variant="outline">Add import</Badge>.
-            </li>
-          </ol>
-        </CollapsibleContent>
-      </Collapsible>
+      <GoogleHelp />
+      <OutlookHelp />
+      <ProtonHelp />
     </div>
   );
 };
+
+const GoogleHelp = () => (
+  <Collapsible>
+    <CollapsibleTrigger asChild>
+      <Button variant="ghost" className="group w-full text-base">
+        Google Calendar
+        <FaChevronDown className="ml-auto group-data-[state=open]:rotate-180" />
+      </Button>
+    </CollapsibleTrigger>
+
+    <CollapsibleContent className="text-muted-foreground px-5">
+      If the Google Calendar button doesn&apos;t work, you can subscribe
+      manually:
+      <ol className="ml-8 list-decimal">
+        <li>Open Google Calendar.</li>
+        <li>
+          Click <Badge variant="outline">+</Badge> next to{" "}
+          <Badge variant="outline">Other calendars</Badge>.
+        </li>
+        <li>
+          Select <Badge variant="outline">From URL</Badge>.
+        </li>
+        <li>Paste the calendar URL above.</li>
+        <li>
+          Click <Badge variant="outline">Add calendar</Badge>.
+        </li>
+      </ol>
+    </CollapsibleContent>
+  </Collapsible>
+);
+const OutlookHelp = () => (
+  <Collapsible>
+    <CollapsibleTrigger asChild>
+      <Button variant="ghost" className="group w-full text-base">
+        Outlook
+        <FaChevronDown className="ml-auto group-data-[state=open]:rotate-180" />
+      </Button>
+    </CollapsibleTrigger>
+    <CollapsibleContent className="text-muted-foreground px-5">
+      If the Outlook button doesn&apos;t work (which is usually the case if you
+      are using the web version), you can subscribe manually:
+      <ol className="ml-8 list-decimal">
+        <li>Open Outlook Calendar.</li>
+        <li>
+          Click <Badge variant="outline">Add calendar</Badge>.
+        </li>
+        <li>
+          Click <Badge variant="outline">Subscribe from web</Badge>.
+        </li>
+        <li>Paste the calendar URL above.</li>
+        <li>
+          Click <Badge variant="outline">Add import</Badge>.
+        </li>
+      </ol>
+    </CollapsibleContent>
+  </Collapsible>
+);
+
+const ProtonHelp = () => (
+  <Collapsible>
+    <CollapsibleTrigger asChild>
+      <Button variant="ghost" className="group w-full text-base">
+        Proton Calendar
+        <FaChevronDown className="ml-auto group-data-[state=open]:rotate-180" />
+      </Button>
+    </CollapsibleTrigger>
+
+    <CollapsibleContent className="text-muted-foreground px-5">
+      For Proton Calendar, you can subscribe manually:
+      <ol className="ml-8 list-decimal">
+        <li>Open Proton Calendar.</li>
+        <li>
+          Click <Badge variant="outline">+</Badge> next to{" "}
+          <Badge variant="outline">My calendars</Badge>.
+        </li>
+        <li>
+          Select <Badge variant="outline">Add calendar from URL</Badge>.
+        </li>
+        <li>Paste the calendar URL above.</li>
+        <li>
+          Click <Badge variant="outline">Add calendar</Badge>.
+        </li>
+      </ol>
+    </CollapsibleContent>
+  </Collapsible>
+);
 
 export default HelpSection;
