98e0073494c01af754d6860cac05b2291dc3f3ed
th-herve <126357296+th-herve@users.noreply.github.com>
2026-09-16 10:11:12 +0200

    feat(front): add navbar to not-found page

--- frontend/app/not-found.tsx
+++ frontend/app/not-found.tsx
@@ -1,15 +1,18 @@
+import DefaultLayout from "@/components/layouts/default-layout";
 import Link from "next/link";
 
 export default function NotFound() {
   return (
-    <div className="flex flex-col h-[90vh] items-center justify-center gap-5">
-      <div className="flex items-center justify-center gap-5">
-        <h2 className="font-date border-muted-foreground border-r pr-10">
-          404
-        </h2>
-        <p>Could not find requested resource</p>
+    <DefaultLayout>
+      <div className="flex h-[90vh] flex-col items-center justify-center gap-5">
+        <div className="flex items-center justify-center gap-5">
+          <h2 className="font-date border-muted-foreground border-r pr-10">
+            404
+          </h2>
+          <p>Could not find requested resource</p>
+        </div>
+        <Link href="/">Return Home</Link>
       </div>
-      <Link href="/">Return Home</Link>
-    </div>
+    </DefaultLayout>
   );
 }
